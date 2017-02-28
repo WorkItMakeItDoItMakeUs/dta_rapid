c118323207ec2696ad87476559768a8184c5eb71
Gareth Rogers <grogers@thoughtworks.com>
2017-02-28 12:11:24 +1100

    Fix todo check and add it to circle

--- _build_scripts/code_check.sh
+++ _build_scripts/code_check.sh
@@ -6,18 +6,25 @@ echo "============================="
 echo "Checking for TODOs and FIXMEs"
 echo "============================="
 
-TODOS=$(grep -irInH --exclude-dir="node_modules" --exclude-dir="*/vendor/*" --exclude-dir="_build_scripts" TODO . | wc -l)
+TODOS=$(grep -irInH --exclude-dir="node_modules" --exclude-dir=".git" --exclude-dir="*/vendor/*" --exclude-dir="_build_scripts" TODO . | wc -l)
 echo -e "Total TODOs:\t$TODOS"
 
-FIXMES=$(grep -irInH --exclude-dir="node_modules" --exclude-dir="*/vendor/*" --exclude-dir="_build_scripts" FIXME . | wc -l)
-echo -e "Total FIXMEs:\t$TODOS"
+FIXMES=$(grep -irInH --exclude-dir="node_modules" --exclude-dir=".git" --exclude-dir="*/vendor/*" --exclude-dir="_build_scripts" FIXME . | wc -l)
+echo -e "Total FIXMEs:\t$FIXMES"
 
 echo ""
 SUM=$(($TODOS + $FIXMES))
 
 if ((SUM > MAX)); then
   echo "Too many TODOs and FIXMEs ($SUM > $MAX)"
+  
+  echo "TODOs"
+  grep -irInH --exclude-dir="node_modules" --exclude-dir="*/vendor/*" --exclude-dir="_build_scripts" TODO .
+  echo "FIXMEs"
+  grep -irInH --exclude-dir="node_modules" --exclude-dir="*/vendor/*" --exclude-dir="_build_scripts" FIXME .
+  echo ""
   echo "Failing build..."
+
   exit 2
 fi
 
