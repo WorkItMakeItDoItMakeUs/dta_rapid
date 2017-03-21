8163e32a530f441cee466c5b26fdeabfb1eb4741
@chambois <mwchamberlain@gmail.com>
2017-03-21 11:20:53 +1100

    @chambois: Spike PikADay

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,30 @@
 'use strict';
 
-
 const scrollForm = require('./scroll-form');
+const Pikaday = require('pikaday');
 
 scrollForm.init();
+
+const initPickADate = () => {
+    $('input[type=date]').pickadate();
+}
+
+const initPikADay = () => {
+
+  new Pikaday({
+    field: document.querySelectorAll('input[type=date]')[0]
+  });
+
+  // turn it into a text type so Chrome doesn't show it's native picker
+  $('input[type="date"]').attr('type','text');
+}
 
 $('document').ready(() => {
   //const isNotMobile = ('ontouchstart' in document.documentElement && navigator.userAgent.match(/Mobi/));
   const isNotMobile = !(navigator.userAgent.match(/Mobi/));
 
   if (isNotMobile) {
-    $('input[type=date]').pickadate();
+    initPikADay();
   }
 });
 
