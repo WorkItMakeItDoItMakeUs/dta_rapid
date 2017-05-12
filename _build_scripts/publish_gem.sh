e7447556f413e5f549d11c5b7d4a1d29fd75f266
@chambois <mwchamberlain@gmail.com>
2017-05-12 16:49:26 +1000

    @chambois: Add data-attr for radio button to retrieve name easily

--- _build_scripts/publish_gem.sh
+++ _build_scripts/publish_gem.sh
@@ -1,16 +1,51 @@
 #! /bin/bash
 
 # echo current version
-version=`gem specification dta_rapid version | awk 'match($0, /version(.*)/) { print substr( $2, RSTART, RLENGTH )}'`
-IFS='.' read -r -a version_array <<< "$version"
+ascertain_version() {
+	version=`gem specification dta_rapid version | awk 'match($0, /version(.*)/) { print substr( $2, RSTART, RLENGTH )}'`
+	IFS='.' read -r -a version_array <<< "$version"
 
-patch_version=${version_array[0]}
-minor_version=${version_array[1]}
-major_version=${version_array[2]}
+	major_version=${version_array[0]}
+	minor_version=${version_array[1]}
+	patch_version=${version_array[2]}
+}
 
-echo $patch_version
-echo $minor_version
-echo $major_version
+show_menu() {
+	echo 'Current gem version: ' $version
+	echo
+  echo Publish new version of gem
+  echo ==========================
+  echo
+  echo "  1. Release new Major version (1.x.x > 2.x.x)"
+  echo "  2. Release new Minor version (x.1.x > x.2.x)"
+  echo "  3. Release new Patch version (x.x.1 > x.x.2)"
+	echo
+  echo "  CTRL+C: Quit"
+  echo
+  read -p "Please select an option: " REPLY </dev/tty
+}
+
+handle_option() {
+  case "$1" in
+    major|1)
+			echo 'new Major'
+      ;;
+    minor|2)
+			echo 'new Minor'
+      ;;
+    patch|3)
+			echo 'new Patch'
+      ;;
+    *)
+      echo Invalid Option!
+      echo "Available options are: <major/minor/patch>"
+      exit 1
+  esac
+}
+
+ascertain_version
+show_menu
+handle_option $REPLY
 
 #commit update to gemspec
 #gem build dta_rapid.gemspec
