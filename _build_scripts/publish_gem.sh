294b663f3ecfbbc4169ff8148d64fabd4b3721c0
@chambois <mwchamberlain@gmail.com>
2017-05-16 09:53:31 +1000

    @chambois: bump gem version

--- _build_scripts/publish_gem.sh
+++ _build_scripts/publish_gem.sh
@@ -2,7 +2,8 @@
 
 # echo current version
 ascertain_version() {
-	version=`gem specification dta_rapid version | awk 'match($0, /version(.*)/) { print substr( $2, RSTART, RLENGTH )}'`
+	#version=`gem specification dta_rapid version | awk 'match($0, /version(.*)/) { print substr( $2, RSTART, RLENGTH )}'`
+	version=`cat dta_rapid.gemspec | sed -n 's/.*version.*"\(.*\)"/\1/p'`
 	IFS='.' read -r -a version_array <<< "$version"
 
 	major_version=${version_array[0]}
@@ -11,15 +12,15 @@ ascertain_version() {
 }
 
 show_menu() {
-	echo 'Current gem version: ' $version
-	echo
+  echo 'Current gem version: ' $version
+  echo
   echo Publish new version of gem
   echo ==========================
   echo
   echo "  1. Release new Major version (1.x.x > 2.x.x)"
   echo "  2. Release new Minor version (x.1.x > x.2.x)"
   echo "  3. Release new Patch version (x.x.1 > x.x.2)"
-	echo
+  echo
   echo "  CTRL+C: Quit"
   echo
   read -p "Please select an option: " REPLY </dev/tty
@@ -28,13 +29,14 @@ show_menu() {
 handle_option() {
   case "$1" in
     major|1)
-			echo 'new Major'
+      echo 'new Major'
       ;;
     minor|2)
-			echo 'new Minor'
+      echo 'new Minor'
       ;;
     patch|3)
-			echo 'new Patch'
+      echo 'Releasing new Patch version'
+      
       ;;
     *)
       echo Invalid Option!
